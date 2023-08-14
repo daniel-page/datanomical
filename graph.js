acafa5917691c4189da01365019d86003d86cca9
Daniel Page <dwpage98@gmail.com>
2023-08-14 19:35:11 +1200

    Graph styling, electron toolbar hidden

--- graph.js
+++ graph.js
@@ -1,21 +1,62 @@
 const ctx = document.getElementById('test');
 
 new Chart(ctx, {
-	type: 'bar',
+	type: 'line',
+	borderColor: "#FFFFFF",
+
 	data: {
-		labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+		labels: [1, 2, 3, 4, 5, 6],
 		datasets: [{
+			backgroundColor: "rgb(255, 99, 132)",
+			borderColor: "rgb(255, 99, 132)",
 			label: '# of Votes',
+			borderWidth: 3,
 			data: [12, 19, 3, 5, 2, 3],
-			borderWidth: 1
 		}]
 	},
 	options: {
+		devicePixelRatio: 2,
+		plugins: {
+			legend: {
+				display: false
+			},
+		},
 		responsive: true,
 		maintainAspectRatio: false,
 		scales: {
 			y: {
-				beginAtZero: true
+				grid: {
+					color: "rgba(255, 255, 255, 0.3)"
+				},
+				ticks: {
+					color: "#FFFFFF",
+				},
+				beginAtZero: true,
+				title: {
+					color: "#FFFFFF",
+					display: true,
+					text: "Received Value",
+					font: {
+						color: "red",
+						size: 16,
+					},
+				},
+			},
+			x: {
+				grid: {
+					color: "rgba(255, 255, 255, 0.3)"
+				},
+				ticks: {
+					color: "#FFFFFF",
+				},
+				title: {
+					color: "#FFFFFF",
+					display: true,
+					text: "Time (s)",
+					font: {
+						size: 16,
+					},
+				},
 			}
 		}
 	}
